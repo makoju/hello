41e6e1740c4a1312460033f8a52fb65e7888962e
Aleksandar Vulovic <aleksandar.vulovic@comtrade.com>
2017-07-26 21:59:38 +0100

    update

--- src/main/java/com/datalex/taf/ui/helpers/ElementHelper.java
+++ src/main/java/com/datalex/taf/ui/helpers/ElementHelper.java
@@ -1,10 +1,7 @@
 package com.datalex.taf.ui.helpers;
 
 import org.apache.logging.log4j.LogManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
