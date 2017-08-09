de63870f50ebda77bc88b7ddae0632284c757df9
jossie saul <jossie.saul@datalex.com>
2017-08-09 09:27:26 +0100

    sanity check feature

--- src/test/java/com/datalex/taf/ui/smoke/SanityTest.java
+++ src/test/java/com/datalex/taf/ui/smoke/SanityTest.java
@@ -3,6 +3,7 @@ package com.datalex.taf.ui.smoke;
 import com.datalex.taf.ui.base.TAFSelenium;
 import com.datalex.taf.ui.data.TestData;
 import com.datalex.taf.ui.pageobjects.searchpage.SearchPage;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,10 +12,16 @@ import org.testng.annotations.Test;
  * Created by jossie.saul on 09/08/2017.
  */
 @Test(groups = {"sanity"})
+@Log4j2
 public class SanityTest {
 
-    @Test(dataProvider = "Data", description = "Basic End to End booking flow")
+    @Test(description = "Basic End to End booking flow")
     public void flightSearchPageReadyTest() throws Exception {
+        log.info("==============================");
+        log.info("==============================");
+        log.info("****      SANITY CHECK    ****");
+        log.info("==============================");
+        log.info("==============================");
         new TAFSelenium().initDriver();
         WebDriver driver = TAFSelenium.getDriver();
         SearchPage searchPage = new SearchPage(driver);
