07262d4cf751f2f573a9d955a24c7f07fe0e19e7
jossie saul <jossie.saul@datalex.com>
2017-08-10 06:06:12 +0100

    adding driver quit with screenshot if error occured

--- src/test/java/com/datalex/taf/ui/smoke/SanityTest.java
+++ src/test/java/com/datalex/taf/ui/smoke/SanityTest.java
@@ -2,10 +2,13 @@ package com.datalex.taf.ui.smoke;
 
 import com.datalex.taf.ui.base.TAFSelenium;
 import com.datalex.taf.ui.data.TestData;
+import com.datalex.taf.ui.helpers.ScreenshotHelper;
 import com.datalex.taf.ui.pageobjects.searchpage.SearchPage;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -14,6 +17,13 @@ import org.testng.annotations.Test;
 @Test(groups = {"sanity"})
 @Log4j2
 public class SanityTest {
+
+    @AfterMethod
+    public void tearDown(ITestResult result) throws Exception {
+        if (!result.isSuccess())
+            new ScreenshotHelper().takeScreenshot(TAFSelenium.getDriver());
+        TAFSelenium.getDriver().quit();
+    }
 
     @Test(description = "Basic End to End booking flow")
     public void flightSearchPageReadyTest() throws Exception {
